800d7b9bff5d8eb7ed1225116a4143ef7f11befc
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-09-23 11:14:46 +0700

    update FPS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import os
 import cv2
 import numpy as np
+import time
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.adapters import common
@@ -15,12 +16,12 @@ modelPath = 'model_edgetpu.tflite'
 labelPath = 'labels.txt'
 
 #some value to insert the Text to CV2
-text = "Text to insert"
+
 font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 1
+font_scale = 0.5
 font_color = (0, 255, 255)  # Text color (Yellow)
-font_thickness = 2
-x, y = 10, 50  # Location of the Text
+font_thickness = 1
+
 
 # This function takes in a TFLite Interptere and Image, and returns classifications
 def classifyImage(interpreter, image):
@@ -39,6 +40,9 @@ def main():
     cap = cv2.VideoCapture(0)
     while cap.isOpened():
         ret, frame = cap.read()
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         if not ret:
             break
 
@@ -47,10 +51,11 @@ def main():
 
         # Classify and display image
         results = classifyImage(interpreter, frame)
-
-        cv2.putText(frame, text, (x, y), font, font_scale, font_color, font_thickness)
-        cv2.imshow('Camera AI using coral', frame)
         print(f'Label: {labels[results[0].id]}, Score: {results[0].score}')
+        text = f'Label: {labels[results[0].id]}, Score: {results[0].score:.2f}'
+        cv2.putText(frame, text, (10, 50), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, f'FPS: {fps}', (10, 30), font, font_scale, font_color, font_thickness)
+        cv2.imshow('Camera AI using coral', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         key = cv2.waitKey(1) & 0xFF
