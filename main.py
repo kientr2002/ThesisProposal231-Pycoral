3a833173e0fa2ae795febad64345bc0fe6790993
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-10-03 15:38:11 +0700

    add fps and CPU Usages

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import os
 import cv2
 import numpy as np
 import time
+import psutil
+
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.adapters import common
 from pycoral.adapters import classify
+from imutils.video import FPS
 
 
 # the TFLite converted to be used with edgetpu
@@ -19,7 +22,7 @@ labelPath = 'labels.txt'
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 font_scale = 0.5
-font_color = (0, 255, 255)  # Text color (Yellow)
+font_color = (0, 0, 0)  # Text color (Yellow)
 font_thickness = 1
 
 
@@ -38,14 +41,13 @@ def main():
     labels = read_label_file(labelPath)
 
     cap = cv2.VideoCapture(0)
+    fps = FPS().start()
     while cap.isOpened():
         ret, frame = cap.read()
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         if not ret:
             break
-
         # Flip image so it matches the training input
         frame = cv2.flip(frame, 1)
 
@@ -53,13 +55,21 @@ def main():
         results = classifyImage(interpreter, frame)
         print(f'Label: {labels[results[0].id]}, Score: {results[0].score}')
         text = f'Label: {labels[results[0].id]}, Score: {results[0].score:.2f}'
+
+        fps.update()
+        fps.stop()
+        fps_on_frame = f'FPS: {round(fps.fps(),2)}'
+        cpu_usages_on_frame = f'CPU Usages: {psutil.cpu_percent()} %'
         cv2.putText(frame, text, (10, 50), font, font_scale, font_color, font_thickness)
-        cv2.putText(frame, f'FPS: {fps}', (10, 30), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, fps_on_frame + ' ' + cpu_usages_on_frame, (10, 70), font, font_scale, font_color, font_thickness)
         cv2.imshow('Camera AI using coral', frame)
+        # CPU Usage
+        print(f'CPU Usages: {psutil.cpu_percent()} %')
+        print(f'FPS: {fps.fps():.2f}')
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         key = cv2.waitKey(1) & 0xFF
-        # Press "Q" Button to shutdown the Program
+        # Press "Q" Button to shut down the Program
         if key == ord('q'):
             break
     cap.release()
