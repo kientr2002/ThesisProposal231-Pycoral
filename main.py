6b8c790c5a6250eb7ed2bf509ca224dcef5b8e40
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-10-12 01:58:40 +0700

    add thread to counter second, add condition to identify image

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import psutil
 import matplotlib.pyplot as plt
 import pycoral as pcr
+import threading
 
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
@@ -31,6 +32,13 @@ font_thickness = 1
 output_dir = 'captured_images'
 os.makedirs(output_dir, exist_ok=True)
 
+# Identification
+counter = 0  # Count Images with the detection over 0.8
+now_Object = ""
+previous_Object = ""
+label_now = ""
+# Biến đếm giây
+seconds = 0
 #The conditional of detection objects
 
 # This function takes in a TFLite Interptere and Image, and returns classifications
@@ -40,11 +48,41 @@ def classifyImage(interpreter, image):
                                              interpolation=cv2.INTER_CUBIC))
     interpreter.invoke()
     return classify.get_classes(interpreter)
+def count_seconds():
+    global seconds
+    while True:
+        time.sleep(1)
+        seconds += 1
 
+def Counter(input):
+    global counter,now_Object,previous_Object, label_now
+    if counter == 0:
+        now_Object = input
+        counter = counter + 1
+    elif counter > 0 and counter < 5:
+        previous_Object = now_Object
+        now_Object = input
+        if now_Object == previous_Object:
+            counter = counter + 1
+        else:
+            previous_Object = ""
+            now_Object = ""
+            counter = 0
+    else:
+        previous_Object = now_Object
+        now_Object = input
+        if now_Object == previous_Object:
+            label_now = now_Object
+        now_Object = ""
+        previous_Object = ""
+        counter = 0
 
-
+count_thread = threading.Thread(target=count_seconds)
+count_thread.daemon = True
+count_thread.start()
 
 def main():
+    global counter, now_Object, previous_Object, label_now
     # Load your model onto the TF Lite Interpreter
     interpreter = make_interpreter(modelPath)
     interpreter.allocate_tensors()
@@ -54,6 +92,7 @@ def main():
     fps = FPS().start()
     frame_times = []
     CPU_Usages = []
+    Scores = []
     pcr.adapters.classify
     while cap.isOpened():
         ret, frame = cap.read()
@@ -65,16 +104,21 @@ def main():
         # Classify and display image
         results = classifyImage(interpreter, frame)
         print(f'Label: {labels[results[0].id]}, Score: {results[0].score}')
+        print(f'Label detected: {label_now} and counter: {counter}')
+        print(seconds)
         text = f'Label: {labels[results[0].id]}, Score: {results[0].score:.2f}'
 
         fps.update()
         fps.stop()
         frame_times.append(round(fps.fps(),2))
+        Scores.append(round(results[0].score, 4))
         fps_on_frame = f'FPS: {round(fps.fps(),2)}'
+
         cpu_usages_on_frame = f'CPU Usages: {psutil.cpu_percent()} %'
         CPU_Usages.append(psutil.cpu_percent())
-        cv2.putText(frame, text, (10, 50), font, font_scale, font_color, font_thickness)
-        cv2.putText(frame, fps_on_frame + ' ' + cpu_usages_on_frame, (10, 70), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, label_now, (10, 50), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, text, (10, 70), font, font_scale, font_color, font_thickness)
+        cv2.putText(frame, fps_on_frame + ' ' + cpu_usages_on_frame, (10, 90), font, font_scale, font_color, font_thickness)
         cv2.imshow('Camera AI using coral', frame)
         # CPU Usage
         print(f'CPU Usages: {psutil.cpu_percent()} %')
@@ -83,10 +127,18 @@ def main():
             # Tạo tên tệp tin duy nhất dựa trên thời gian hiện tại
             timestamp = int(time.time())
             image_filename = os.path.join(output_dir, f'captured_{timestamp}.jpg')
-
             # Lưu ảnh từ frame
             cv2.imwrite(image_filename, frame)
             print(f'Đã chụp ảnh và lưu tại: {image_filename}')
+            #Counter Detection
+            input = labels[results[0].id]
+            Counter(input)
+        else:
+            counter = 0
+            now_Object = ""
+            previous_Object = ""
+            label_now = ""
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         key = cv2.waitKey(1) & 0xFF
@@ -95,10 +147,17 @@ def main():
             break
     cap.release()
     cv2.destroyAllWindows()
+    #Scores
+    plt.plot(Scores)
+    plt.xlabel('Khung hình')
+    plt.ylabel('Score')
+    plt.title('Thông số')
+    plt.show()
+    #FPS
     plt.plot(frame_times)
     plt.xlabel('Khung hình')
     plt.ylabel('FPS')
-    plt.title('Biểu đồ FPS')
+    plt.title('Thông số')
     plt.show()
 if __name__ == '__main__':
     main()
